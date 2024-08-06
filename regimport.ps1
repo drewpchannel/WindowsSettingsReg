c0c3577056c4d43f5d619674438381f9c41e78ad
Drew Poulin <drewpcchannel@gmail.com>
2024-08-06 14:18:03 -0400

    changed to resolving file path, was opening cmd in system32 and not finding settings

--- regimport.ps1
+++ regimport.ps1
@@ -2,6 +2,8 @@
 Invoke-WebRequest https://github.com/drewpchannel/WindowsSettingsReg/archive/refs/heads/main.zip -OutFile .\wsr.zip
 Expand-Archive .\wsr.zip -Force
 
-Start-Process reg.exe -ArgumentList "import .\wsr\WindowsSettingsReg-main\WindowsSettings.reg" -Wait
+$filePath = Resolve-Path ".\wsr\WindowsSettingsReg-main" 
+
+Start-Process reg.exe -ArgumentList "import $filePath\WindowsSettings.reg" -Wait
 
 exit 0
